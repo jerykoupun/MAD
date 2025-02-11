607f4bf1054a771f053936fb6a5e6be214613470
jery koupun <s22129077@student.unklab.ac.id>
2025-02-11 14:22:54 +0800

    Intro Component View, Text, and StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,10 +1,55 @@
-import {Text} from 'react-native'; // react-native itu labrary
+//import core component react-native
+import {Text, View, StyleSheet} from 'react-native'; // react-native itu labrary
 
+//membuat fungsi
 const App = () => {
   return (
     //JSX
-    <Text>Hello World, My Name is Jery Koupun, I'm Student of UK</Text> //teg yang ada di dalam <> mengikuti apa yang di import
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Hello World, My Name is Jery Koupun, I'm Student of UK
+        </Text>
+      </View>
+
+      <View style={styles.containerr}>
+        <Text style={styles.titlee}>Aku suka MAD</Text>
+      </View>
+    </View>
   );
 };
 
+//export component (funsgi)
 export default App;
+
+//initial styleSheet
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 5,
+    pedding: 20,
+    margin: 20,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: '800',
+    color: 'yellow',
+    textAlign: 'center',
+  },
+  containerr: {
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 5,
+    pedding: 20,
+    margin: 20,
+    borderRadius: 50,
+  },
+  titlee: {
+    fontSize: 50,
+    fontWeight: '800',
+    color: 'yellow',
+    textAlign: 'center',
+  },
+});
