5677bddce1457322a073e02973aa76cd694c63c6
jery koupun <s22129077@student.unklab.ac.id>
2025-02-13 14:32:12 +0800

    Adding Componen Image dan ScrollView

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
 //import core component react-native
-import {Text, View, StyleSheet} from 'react-native'; // react-native itu labrary
+import {Text, View, StyleSheet, Image, ScrollView} from 'react-native'; // react-native itu labrary
 
 //membuat fungsi
 const App = () => {
   return (
     //JSX
-    <View>
+    <>
       <View style={styles.container}>
         <Text style={styles.title}>
           Hello World, My Name is Jery Koupun, I'm Student of UK
         </Text>
       </View>
-
-      <View style={styles.containerr}>
-        <Text style={styles.titlee}>Aku suka MAD</Text>
-      </View>
-    </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Text style={styles.subTitle}>From Directory</Text>
+          <Image style={styles.img1} source={require('./Assets/singa.jpg')} />
+          <Text style={styles.subTitle}>From public</Text>
+          <Image
+            style={styles.img1}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+          />
+          <Text style={styles.subTitle}>From Base64</Text>
+          <Image
+            style={styles.img1}
+            source={{
+              uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+            }}
+          />
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -38,18 +54,15 @@ const styles = StyleSheet.create({
     color: 'yellow',
     textAlign: 'center',
   },
-  containerr: {
-    backgroundColor: 'red',
-    borderColor: 'black',
-    borderWidth: 5,
-    pedding: 20,
-    margin: 20,
-    borderRadius: 50,
-  },
-  titlee: {
+  subTitle: {
     fontSize: 50,
     fontWeight: '800',
-    color: 'yellow',
+    color: 'white',
     textAlign: 'center',
+  },
+  img1: {
+    width: 300,
+    height: 300,
+    margin: 20,
   },
 });
