f1e1e7892ea6057c79ca27c6097b5c5dc32a4a6d
jery koupun <s22129077@student.unklab.ac.id>
2025-02-20 00:10:43 +0800

    80%

--- Exercise2.tsx
+++ Exercise2.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Text, View, StyleSheet, ImageBackground, Image} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  ScrollView,
+} from 'react-native';
 
 const Exercise2 = () => {
   return (
     <>
       <View>
         <ImageBackground
-          source={require('./Assets/Background.jpg')}
+          source={require('./Assets/Background2.jpg')}
           style={styles.backgroudimage}
           imageStyle={{borderRadius: 9}}>
           <View>
@@ -16,7 +23,87 @@ const Exercise2 = () => {
       </View>
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
         <Image style={styles.img} source={require('./Assets/Myfoto.jpg')} />
+        <Text style={styles.youngjer}>Young Jerr</Text>
       </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <ImageBackground
+            source={require('./Assets/Background3.jpg')}
+            style={styles.backgroudimage2}
+            imageStyle={{borderRadius: 2}}>
+            <View>
+              <Text style={styles.information}>Name : Koupun, Jery Marten</Text>
+              <Text style={styles.information}>
+                I am a student at the Faculty of Computer Science, Universitas
+                Klabat, majoring in Information Systems. I have expertise in
+                business planning and enjoy logical and critical thinking.
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+
+        <View>
+          <ImageBackground
+            source={require('./Assets/Background4.jpg')}
+            style={styles.backgroudimage2}
+            imageStyle={{borderRadius: 2}}>
+            <View>
+              <Text style={styles.information2}>Born: June 10, 2006</Text>
+              <Text style={styles.information2}>
+                I was born in the city of Ambon, in a village called Layeni. I
+                was born during the daytime. However, since I moved to Manado in
+                2010, I have never returned to my birthplace.
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+
+        <View>
+          <ImageBackground
+            source={require('./Assets/Background5.jpg')}
+            style={styles.backgroudimage2}
+            imageStyle={{borderRadius: 2}}>
+            <View>
+              <Text style={styles.information}>Education</Text>
+              <Text style={styles.information}>
+                I started my education at GMIM 85 Wori Kindergarten. After that,
+                I continued my elementary education at GMIM 85 Wori Elementary
+                School. I then pursued my junior high school education at Advent
+                6 Panda Junior High School. After graduating, I continued to
+                vocational high school at SMK Negeri 1 Wori.
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+
+        <View>
+          <ImageBackground
+            source={require('./Assets/Background7.jpg')}
+            style={styles.backgroudimage2}
+            imageStyle={{borderRadius: 2}}>
+            <View>
+              <Text style={styles.information2}>Skills</Text>
+              <Text style={styles.information2}>
+                Skills I have several skills related to the field of study I am
+                currently pursuing. I enjoy analysis, business planning, data
+                mining, and several other areas of expertise.
+              </Text>
+            </View>
+          </ImageBackground>
+        </View>
+
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            source={require('./Assets/E-Business.jpg')}
+            style={styles.img}
+            imageStyle={{borderRadius: 2}}></Image>
+          <Text style={styles.youngjer}>Klabat Set Up</Text>
+          <Text style={styles.youngjer2}>
+            Klabat Set Up is one of the E-Business projects that I created.
+          </Text>
+        </View>
+      </ScrollView>
     </>
   );
 };
@@ -27,20 +114,49 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     color: 'black',
+    fontWeight: 'bold',
   },
   backgroudimage: {
     marginHorizontal: 20,
     marginTop: 10,
     padding: 15,
   },
+  backgroudimage2: {
+    marginHorizontal: 20,
+    marginTop: 50,
+    padding: 15,
+  },
   img: {
-    width: 250,
-    height: 250,
-    marginTop: 10,
+    width: 180,
+    height: 180,
+    marginTop: 50,
     marginBottom: 10,
     marginLeft: 10,
     marginRight: 10,
     borderRadius: 250 / 2,
-    borderColor: 'blue',
+    borderWidth: 2,
+    borderColor: '#76d1ff',
+  },
+  youngjer: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  youngjer2: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: 'white',
+  },
+  information: {
+    margin: 10,
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'justify',
+  },
+  information2: {
+    margin: 10,
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'justify',
   },
 });
