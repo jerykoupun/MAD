02f408f13557b679aefb5e2547b98546e1fdcc44
jery koupun <s22129077@student.unklab.ac.id>
2025-04-15 12:36:22 +0800

    Slicing Sign Up Page

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import SplashScreen from './src/pages/Splashscreen';
 import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
 const App = () => {
-  return <SignIn />;
+  return <SignUp />;
 };
 
 export default App;
--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,4 @@
 import Logo from './Logo';
+import Arrow from './arrow';
 
-export {Logo};
+export {Logo, Arrow};
